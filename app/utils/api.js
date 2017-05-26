4dd86f9afce06a283b5c176f0b2d91b98ec7c95a
Tony Brown <tony@tonybrown.me>
2017-05-26 19:07:39 -0400

    make a get request to Github api with axios and display the json

---
+++ app/utils/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+module.exports = {
+  fetchPopularRepos: function (language) {
+    let encodedURI =
+      window.encodeURI(
+        'https://api.github.com/search/repositories?q=stars:>1+language:'+
+        language + '&sort=starts&order=desc&type=repositories'
+      )
+
+      return axios.get(encodedURI)
+        .then((response) => {
+          return response.data.items
+        })
+  }
+}
